6db533b1b030af1c1be9f48935d261b214714e2a
Hanad Mohamed <hanaddaahir124@gmail.com>
2024-07-28 21:10:15 +0300

    feat: add disease explanation and treatment recommendations.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93da26d210ac4649"; }
+/******/ 		__webpack_require__.h = function() { return "b1be1f7f7b729e3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
